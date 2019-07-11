052e6f75a4ba426f99de804fdb996b1399afaf62
CHATHURA SAKUN ELLAWALA <sakun@villvay.com>
2019-07-11 10:35:21 +0530

    save/delete/get sessionToken methods in authaction

--- app/redux/KeyConstants.js
+++ app/redux/KeyConstants.js
@@ -9,4 +9,26 @@ export default keyMirror({
   GET_LIST_REQUEST: null,
   GET_LIST_SUCCESS: null,
   GET_LIST_FAILURE: null,
+
+  //auth actions
+  SESSION_TOKEN_REQUEST: null,
+  SESSION_TOKEN_SUCCESS: null,
+  SESSION_TOKEN_FAILURE: null,
+
+  DELETE_TOKEN_REQUEST: null,
+  DELETE_TOKEN_SUCCESS: null,
+
+  LOGIN: null,
+
+  LOGOUT_REQUEST: null,
+  LOGOUT_SUCCESS: null,
+  LOGOUT_FAILURE: null,
+
+  LOGIN_REQUEST: null,
+  LOGIN_SUCCESS: null,
+  LOGIN_FAILURE: null,
+
+  RESET_PASSWORD_REQUEST: null,
+  RESET_PASSWORD_SUCCESS: null,
+  RESET_PASSWORD_FAILURE: null,
 });
