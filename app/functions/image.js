dec5c1436e9cf4ce785dbd244c9bc1527acb0386
CHATHURA SAKUN ELLAWALA <sakun@villvay.com>
2019-07-11 16:17:26 +0530

    add default logo

--- app/functions/image.js
+++ app/functions/image.js
@@ -3,7 +3,8 @@ const images = {
   'plus': require('../assets/plus.png'),
   'minus': require('../assets/minus.png'),
   'delete': require('../assets/delete.png'),
-  'menu': require('../assets/ic_menu.png')
+  'menu': require('../assets/ic_menu.png'),
+  'defaultLogo': require('../assets/defUser.png')
 };
 
 export default images;
